f7e056db106e2687701cb63ddf714e1a77fe566a
zarlashta manana <zarlashta27@yahoo.com>
2018-07-20 17:20:35 -0400

    Added the artist name in spotify

--- liri.js
+++ liri.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 //Configurations/global varaibles
 var Twitter = require("twitter");
 
-//var Spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 var keys = require("./keys.js");
 
 //Setting up the keys
 var client = new Twitter(keys.twitter);
 
-//var spotify = new Spotify(keys.Spotify);
+var spotify = new Spotify(keys.spotify);
 
 //Reference Codes for Twitter
 // var params = {screen_name: 'zaro14918904'};
@@ -38,27 +38,39 @@ var client = new Twitter(keys.twitter);
 //Function Section
 
 //===========================Beginning of Twitter===================
-
-client.get('statuses/user_timeline', {count: 20}, function(error, tweets, response){
-  //If you get an error, print an error message
-
-  //If you get the response, print the 20 tweets
-
-  console.log(tweets);
-
-})
-
-
-
-
-
+function mytweets() {
+  client.get('statuses/user_timeline', {count: 20}, function(error, tweets, response){
+    //If you get an error, print an error message
+    if (error) {
+      console.log("Sorry, Try Again!");
+    }
+    //If you get the response, print the 20 tweets
+    
+    for (let i= 0; i< 20; i++) {
+       console.log(tweets[i].text);
+    }
+  
+  })
+}
 
 //=========================End of Twitter=============================
 
 
 
 //=========================Beginning of Spotify=======================
+spotify.search({ type: 'track', query: 'Hit me baby one more' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  //Need to list the name of the artist
+  console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name,null, 2)); 
+  //list the  The song's name
 
+  //List the preview link of the song from Spotify
+
+  //List the album that the song is from
+  });
+  
 
 
 
